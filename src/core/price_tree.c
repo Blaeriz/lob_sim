2e74927d34eda5929e332cd48abee60779cef1ff
Blaeriz <pinglehrishikesh@yahoo.in>
2026-01-08 02:51:49 +0530

    feat(price_tree.c): added right_rotate and fixed asserts

--- src/core/price_tree.c
+++ src/core/price_tree.c
@@ -105,24 +105,47 @@ price_level_t *pt_max(const price_tree_t *t) {
 //LEFT ROTATE TREE
 static void left_rotate(price_tree_t *t, price_node_t *x) {
     price_node_t *nil = &t->nil;
-    if (x->right != nil) {
-        price_node_t *y = x->right;
-        x->right = y->left;
-        if (y->left != nil) {
-            y->left->parent = x;
-        }
-        y->parent = x->parent;
-        if (x->parent == nil) {
-            t->root = y;
-        } else if (x == x->parent->left) {
-            x->parent->left = y;
-        } else {
-            x->parent->right = y;
-        }
-        y->left = x;
-        x->parent = y;
-    } else {
-        assert(x != nil);
-        assert(x->right != nil);
+
+    assert(x != nil);
+    assert(x->right != nil);
+
+    price_node_t *y = x->right;
+    x->right = y->left;
+    if (y->left != nil) {
+        y->left->parent = x;
     }
+    y->parent = x->parent;
+    if (x->parent == nil) {
+        t->root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+//RIGHT ROTATE TREE
+static void right_rotate(price_tree_t *t, price_node_t *x) {
+    price_node_t *nil = &t->nil;
+
+    assert(x != nil);
+    assert(x->left != nil);
+
+    price_node_t *y = x->left;
+    x->left = y->right;
+    if (y->right != nil) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nil) {
+        t->root = y;
+    } else if (x == x->parent->right) {
+        x->parent->right = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->right = x;
+    x->parent = y;
 }
