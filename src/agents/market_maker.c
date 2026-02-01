0360614258cd928e72f73516fbecb687e1c37925
Blaeriz <pinglehrishikesh@yahoo.in>
2026-02-01 18:30:56 +0530

    feat(market_maker): wrote create and destroy functions
    fixed spelling mistake in struct and defined _GNU_SOURCE

--- src/agents/market_maker.c
+++ src/agents/market_maker.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include "agents/market_maker.h"
 #include "core/book.h"
 #include "core/price_tree.h"
@@ -15,7 +16,7 @@ typedef struct {
   qty_t max_inventory;
   order_id_t active_bid_id;
   order_id_t active_ask_id;
-} marker_maker_state_t;
+} market_maker_state_t;
 
 
 static void mm_step(agent_t *agent, order_book_t *book, timestamp_t now) {
@@ -26,8 +27,39 @@ static void mm_step(agent_t *agent, order_book_t *book, timestamp_t now) {
 agent_t *market_maker_create(agent_id_t id) {
   agent_t *a = malloc(sizeof(agent_t));
   if (!a) return NULL;
+  market_maker_state_t *state = malloc(sizeof(market_maker_state_t));
+  if (!state) {
+    free(a);
+    return NULL;
+  }
+
+  // STATE
+  state->next_order_id = 1;
+  state->rng_seed = time(NULL) + id;
+  state->half_spread = 5;
+  state->order_qty = 10;
+  state->inventory = 0;
+  state->max_inventory = 100;
+  state->active_ask_id = 0;
+  state->active_bid_id = 0;
+
+  // AGENT
+
   a->id = id;
   a->step = mm_step;
-  a->state = NULL;
+  a->state = state;
+
   return a;
+}
+
+void market_maker_destroy(agent_t *agent) {
+  if (!agent) {
+    return;
+  }
+
+  if (agent->state) {
+    free(agent->state);
+  }
+
+  free(agent);
 }
